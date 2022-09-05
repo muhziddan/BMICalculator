4f7cdfa88accc22b230b7d69e79c6523bb0527fe
muhammad ziddan hidayatullah <ziddan.mz@gmail.com>
2022-09-05 16:35:26 +0700

    remove unecessary self call

--- BMICalculator/Model/Calculator.swift
+++ BMICalculator/Model/Calculator.swift
@@ -19,11 +19,11 @@ struct Calculator {
     
     mutating func objectInitiator(_ bmiValue: Float) {
         switch bmiValue {
-        case _ where bmiValue < 18.5: self.bmi = CalculatorModel(value: bmiValue, suggestion: "You need to eat more!", color: UIColor(red: 0.64, green: 0.80, blue: 0.95, alpha: 1.00))
-        case 18.5...22.9: self.bmi = CalculatorModel(value: bmiValue, suggestion: "You are defenitely in perfect shape!", color: UIColor(red: 0.33, green: 0.96, blue: 0.45, alpha: 1.00))
-        case 23...24.9: self.bmi = CalculatorModel(value: bmiValue, suggestion: "Just need a lil bit of effort to getting you in shape", color: UIColor(red: 0.98, green: 0.84, blue: 0.27, alpha: 1.00))
-        case 25...30: self.bmi = CalculatorModel(value: bmiValue, suggestion: "Now you need to get your self up and change your habbit!", color: UIColor(red: 0.96, green: 0.72, blue: 0.34, alpha: 1.00))
-        case _ where bmiValue > 30: self.bmi = CalculatorModel(value: bmiValue, suggestion: "You need to do hard diet and lots of workout!", color: UIColor(red: 0.97, green: 0.38, blue: 0.37, alpha: 1.00))
+        case _ where bmiValue < 18.5: bmi = CalculatorModel(value: bmiValue, suggestion: "You need to eat more!", color: UIColor(red: 0.64, green: 0.80, blue: 0.95, alpha: 1.00))
+        case 18.5...22.9: bmi = CalculatorModel(value: bmiValue, suggestion: "You are defenitely in perfect shape!", color: UIColor(red: 0.33, green: 0.96, blue: 0.45, alpha: 1.00))
+        case 23...24.9: bmi = CalculatorModel(value: bmiValue, suggestion: "Just need a lil bit of effort to getting you in shape", color: UIColor(red: 0.98, green: 0.84, blue: 0.27, alpha: 1.00))
+        case 25...30: bmi = CalculatorModel(value: bmiValue, suggestion: "Now you need to get your self up and change your habbit!", color: UIColor(red: 0.96, green: 0.72, blue: 0.34, alpha: 1.00))
+        case _ where bmiValue > 30: bmi = CalculatorModel(value: bmiValue, suggestion: "You need to do hard diet and lots of workout!", color: UIColor(red: 0.97, green: 0.38, blue: 0.37, alpha: 1.00))
         default: print("Undefined")
         }
     }
